8b77e05b54b381455681b648ff84b675e4ad3b0d
Oleh Lukash <lukasholeg02@gmail.com>
2024-12-14 20:24:25 +0300

    make build

---
+++ index.f5a3a1ec.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var t=e.querySelector(":nth-child(2)"),l=document.createElement(t.tagName);l.textContent=t.textContent,e.lastElementChild.before(l)});
+//# sourceMappingURL=index.f5a3a1ec.js.map
